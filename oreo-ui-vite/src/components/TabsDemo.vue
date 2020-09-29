76c1b91fa6d6c2310238631d769595e8bdfe9c29
GHkmmm <hgmkk2299@icloud.com>
2020-09-29 17:37:29 +0800

    创建Tab和Tabs组件

--- oreo-ui-vite/src/components/TabsDemo.vue
+++ oreo-ui-vite/src/components/TabsDemo.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    这是Tabs组件
+    <h1>Tabs示例</h1>
+    <h2>示例1</h2>
+    <or-tabs>
+      <or-tab title="导航1">内容1</or-tab>
+      <or-tab title="导航2">内容2</or-tab>
+    </or-tabs>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import OrTabs from '../lib/Tabs.vue';
+import OrTab from '../lib/Tab.vue';
 
+export default {
+  name: 'TabsDemo',
+  components: {
+    OrTabs,
+    OrTab
+  }
 }
 </script>
 
