fb5c9581c9b1b6a8b47ac296930230a518a9ac6d
GHkmmm <hgmkk2299@icloud.com>
2020-09-30 15:49:02 +0800

    渲染Tabs内容

--- oreo-ui-vite/src/lib/Tabs.vue
+++ oreo-ui-vite/src/lib/Tabs.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    <slot></slot>
+    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    <!-- <slot></slot> -->
   </div>
 </template>
 
 <script>
+import OrTab from './Tab.vue';
+
 export default {
-  name: 'OrTabs'
+  name: 'OrTabs',
+  setup(props, context) {
+    const defaults = context.slots.default()
+    
+    defaults.forEach((tag)=>{
+      if(tag.type !== OrTab) {
+        throw new Error('The child component of OrTabs must be OrTab')
+      }
+    })
+
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+
+    return {
+      defaults,
+      titles
+    }
+  }
 }
 </script>
 
