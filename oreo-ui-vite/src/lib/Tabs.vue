6338cf8406ea9c3cf49e9cf5a59abd1023af5773
GHkmmm <hgmkk2299@icloud.com>
2020-09-30 17:24:35 +0800

    显示选中内容

--- oreo-ui-vite/src/lib/Tabs.vue
+++ oreo-ui-vite/src/lib/Tabs.vue
@@ -1,18 +1,34 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
-    <!-- <slot></slot> -->
+  <div class="or-tabs">
+    <div class="or-tabs-nav">
+      <div class="or-tabs-nav-item" 
+           :class="{selected: currentIndex === index}"
+           @click="changeTab(index)" 
+           v-for="(t, index) in titles" 
+           :key="index">{{t}}</div>
+    </div>
+    <div class="or-tabs-content">
+      <component v-for="(c, index) in defaults" 
+                 :is="c" 
+                 :key="index"
+                 class="or-tabs-content-item"
+                 :class="{selected: currentIndex === index}" />
+    </div>
   </div>
 </template>
 
 <script>
 import OrTab from './Tab.vue';
+import { ref } from 'vue';
 
 export default {
   name: 'OrTabs',
   setup(props, context) {
+    let currentIndex = ref(0)
     const defaults = context.slots.default()
+    const changeTab = (index)=>{
+      currentIndex.value = index
+    } 
     
     defaults.forEach((tag)=>{
       if(tag.type !== OrTab) {
@@ -26,12 +42,45 @@ export default {
 
     return {
       defaults,
-      titles
+      titles,
+      currentIndex,
+      changeTab
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+$blue: #0071e3;
+$color: #333;
+$border-color: #d9d9d9;
 
+.or-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
 </style>
