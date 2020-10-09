ef9562631873fec9297c1e84cf9bd9a91388dcb1
GHkmmm <hgmkk2299@icloud.com>
2020-10-09 16:19:07 +0800

    创建rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+// 请先安装 rollup-plugin-esbuild rollup-plugin-vue rollup-plugin-scss sass rollup-plugin-terser
+
+import esbuild from 'rollup-plugin-esbuild'
+import vue from 'rollup-plugin-vue'
+import scss from 'rollup-plugin-scss'
+import dartSass from 'sass';
+import { terser } from "rollup-plugin-terser"
+
+export default {
+  input: 'src/lib/index.ts',
+  output: [{
+    globals: {
+      vue: 'Vue'
+    },
+    name: 'Oreo',
+    file: 'dist/lib/oreo.js',
+    format: 'umd',
+    plugins: [terser()]
+  }, {
+    name: 'Oreo',
+    file: 'dist/lib/oreo.esm.js',
+    format: 'es',
+    plugins: [terser()]
+  }],
+  plugins: [
+    scss({ include: /\.scss$/, sass: dartSass }),
+    esbuild({
+      include: /\.[jt]s$/,
+      minify: process.env.NODE_ENV === 'production',
+      target: 'es2015' 
+    }),
+    vue({
+      include: /\.vue$/,
+    })
+  ],
+}
