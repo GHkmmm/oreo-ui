6bb2eb29d872de600d99af9bfcb1d9c2423525d7
GHkmmm <hgmkk2299@icloud.com>
2020-10-10 13:30:02 +0800

    部署码云

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:GHkmmm/oreo-ui-website.git &&
+git remote add origin git@gitee.com:ghkmmm/oreo-neuui.git &&
 git push origin master -f &&
 cd -
 echo https://ghkmmm.github.io/oreo-ui-website
